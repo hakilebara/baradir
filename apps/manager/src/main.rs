4336a124680769b7af0813b817c98e4b68715115
hakilebara <frederic.soumare@gmail.com>
2026-09-15 14:58:03 +0200

    Refactor manager app to list and upload apps
    - define a stub app-install interface with WIT
    - implmeent listing of installed apps
    - implement upload of a wasi-http wasm component app

--- apps/manager/src/main.rs
+++ apps/manager/src/main.rs
@@ -1,25 +1,49 @@
-use maud::html;
-use wstd::http::{Body, Request, Response, Result, StatusCode};
+use axum::{Router, extract::Multipart, response::Redirect, routing::get, routing::post};
+use maud::{Markup, html};
 
-#[wstd::http_server]
-async fn main(req: Request<Body>) -> Result<Response<Body>> {
-    match req.uri().path() {
-        "/" => home(req).await,
-        _ => not_found(req).await,
+mod bindings {
+    wit_bindgen::generate!({
+        path: "wit/world.wit",
+    });
+}
+
+use bindings::{get_domain, install_app_from_binary, list_apps};
+
+#[wstd_axum::http_server]
+fn main() -> Router {
+    Router::new().route("/", get(home)).route("/", post(upload))
+}
+
+async fn home() -> Markup {
+    let apps: Vec<String> = list_apps();
+    let domain: String = get_domain();
+    html! {
+        h1 { "Manager App" }
+        ul {
+            @for name in &apps {
+                li {
+                    a href={ "http://" (name) "." (domain) } {(name)}
+                }
+            }
+        }
+        form method="post" enctype="multipart/form-data" {
+            input type="text" name="name" placeholder="App name" required;
+            input type="file" name="file" accept=".wasm";
+            input type="submit" value="Upload";
+        }
     }
 }
 
-async fn home(_req: Request<Body>) -> Result<Response<Body>> {
-    let message = "I AM THE MANAGER\n".to_string();
-    let markup = html! {
-        h1 { (message) "!!!" }
-    };
-    Ok(Response::new(markup.into_string().into()))
-}
-
-async fn not_found(_req: Request<Body>) -> Result<Response<Body>> {
-    Ok(Response::builder()
-        .status(StatusCode::NOT_FOUND)
-        .body(().into())
-        .expect("builder must succeed"))
+async fn upload(mut multipart: Multipart) -> Redirect {
+    let mut name = String::new();
+    let mut bytes = Vec::new();
+    while let Some(field) = multipart.next_field().await.unwrap() {
+        match field.name() {
+            Some("name") => name = field.text().await.unwrap(),
+            Some("file") => bytes = field.bytes().await.unwrap().to_vec(),
+            _ => (),
+        }
+    }
+    let _url = install_app_from_binary(&name, &bytes).unwrap();
+    Redirect::to("/")
 }
